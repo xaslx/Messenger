b7ba4ca774e835146ca14017c4712f4c1b76a69a
Maksim <knyazev.maksim2852@gmail.com>
2024-10-17 12:20:25 +0400

    add auth func

---
+++ src/auth/auth.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends
+from jose import jwt
+from passlib.context import CryptContext
+from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.repositories.user import UserRepository
+from src.schemas.user import UserOut
+from config import settings
+from database import get_async_session
+from src.models.user import User
+from exceptions import IncorrectEmailOrPasswordException
+
+pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode: dict = data.copy()
+    expire: datetime = datetime.utcnow() + timedelta(days=15)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.SECRET_ALGORITHM)
+
+    return encode_jwt, expire
+
+
+async def authenticate_user(
+    email: EmailStr,
+    password: str,
+    async_db: AsyncSession = Depends(get_async_session),
+) -> UserOut:
+
+    user: User = await UserRepository.find_one_or_none(email=email, session=async_db)
+    if not (user and verify_password(password, user.hashed_password)):
+        raise IncorrectEmailOrPasswordException
+    return user
---
+++ src/auth/dependencies.py
@@ -0,0 +1,62 @@
+from fastapi import Depends, Request
+from jose import ExpiredSignatureError, JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.user import User
+
+from src.repositories.user import UserRepository
+
+from config import settings
+from database import get_async_session
+from exceptions import (
+    IncorrectTokenException,
+    UserIsNotPresentException,
+)
+from redis_init import redis
+from src.schemas.user import UserOut
+
+
+
+def get_token(request: Request):
+    token: str = request.cookies.get("user_access_token")
+    if not token:
+        return None
+    return token
+
+
+def valid_token(token: str):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.SECRET_ALGORITHM)
+    except ExpiredSignatureError:
+        return None
+    except JWTError:
+        raise IncorrectTokenException
+    return payload
+
+
+async def get_current_user(
+    async_db: AsyncSession = Depends(get_async_session),
+    token: str = Depends(get_token),
+) -> User:
+    if not token:
+        return None
+    payload = valid_token(token=token)
+    user_personal_link: str = payload.get("sub")
+    user_data: None | str = await redis.get(user_personal_link)
+    if user_data:
+        user: UserOut = UserOut.model_validate_json(user_data)
+    else:
+        user: User = await UserRepository.find_one_or_none(
+            personal_link=user_personal_link, session=async_db
+        )
+        user_out = UserOut.model_validate(user)
+        if user:
+            await redis.set(user_personal_link, user_out.model_dump_json(), ex=600)
+
+    if not user_personal_link:
+        raise UserIsNotPresentException
+    if not user:
+        return None
+    return user
+
+
