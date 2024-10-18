3f63d7ad34a9cc1ad814af42d464b17269940d3d
Maksim <knyazev.maksim2852@gmail.com>
2024-10-18 20:21:28 +0400

    update routers

--- src/routers/main.py
+++ src/routers/main.py
@@ -1,7 +1,9 @@
-import uuid
-from fastapi import APIRouter, HTTPException, Request, Depends, WebSocket, WebSocketDisconnect
-from fastapi.responses import JSONResponse
+from datetime import datetime
+from fastapi import APIRouter, Request, Depends, WebSocket, WebSocketDisconnect
+from src.models.chat import Message
 from src.models.user import User
+from src.repositories.user import UserRepository
+from src.schemas.message import MessageCreate, MessageRead
 from src.utils.jinja_template import templates
 from database import get_async_session
 from src.auth.dependencies import get_current_user
@@ -9,51 +11,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 from src.schemas.user import UserOut
 from redis_init import redis
-
-
-async def create_session(user_id: str):
-    session_id = str(uuid.uuid4())  # Генерация уникального идентификатора сессии
-    await redis.set(session_id, user_id)  # Сохранение сессии в Redis
-    return session_id
-
-# Функция для получения сессии
-async def get_session(session_id: str):
-    user_id = await redis.get(session_id)
-    if user_id is None:
-        raise HTTPException(status_code=404, detail="Session not found")
-    return user_id
-
-# Функция для удаления сессии
-async def delete_session(session_id: str):
-    await redis.delete(session_id)
-
+from src.repositories.chat import ChatRepository
+from logger import logger
 
 
 main_router: APIRouter = APIRouter(
     tags=['Мессенджер']
 )
 
+active_connections: dict[int, WebSocket] = {}
 
-people = [
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=1),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=2),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=3),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=4),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=5),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=6),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=7),
-    UserOut(name='Maksim', surname='Ivanov', email='gregrger@gmail.com', id=8),
-]
 
 @main_router.get('/')
 async def get_main_page(
     request: Request, 
-    user: Annotated[User, Depends(get_current_user)]):
+    user: Annotated[User, Depends(get_current_user)],
+    session: Annotated[AsyncSession, Depends(get_async_session)]):
 
+    all_users: list[UserOut] = await UserRepository.find_all(session=session)
+  
     return templates.TemplateResponse(
         request=request,
         name='base.html',
-        context={'user': user, 'people': people}
+        context={'user': user, 'people': all_users}
     )
 
 
@@ -62,6 +42,7 @@ async def get_myprofile_template(
     request: Request,
     user: Annotated[User, Depends(get_current_user)]
 ):
+    
     return templates.TemplateResponse(
         request=request,
         name='me.html',
@@ -70,37 +51,95 @@ async def get_myprofile_template(
 
 
 
-@main_router.get('/dialogs/{dialog_id}')
+@main_router.get('/messages/{user_id}')
 async def get_dialog_by_id(
-    request: Request, 
-    dialog_id: int,
-    session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(get_current_user)
+    user_id: int,
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+    user: Annotated[User, Depends(get_current_user)]
 ):
-    s = 5
-    if dialog_id == s:
 
+    if user:
+        
+        res: Message = await ChatRepository.get_messages_between_users(user_id_1=user_id, user_id_2=user.id, session=session)
+        user_2: User = await UserRepository.find_one_or_none(session=session, id=user_id)
+
+
+        from_orm: MessageRead = [MessageRead.model_validate(i) if res else None for i in res]
         return {
-        "user": {
-            "id": 2,
-            "name": "Иван",
-            "surname": "Иванов"
-        },
-        "messages": [
-            {
-                "senderName": "Иван",
-                "text": "Привет! Как дела?"
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "surname": user.surname
             },
-            {
-                "senderName": "Вы",
-                "text": "Привет, Иван! Все хорошо, а у тебя?"
+            "partner": {
+                "id": user_2.id,
+                "name": user_2.name,
+                "surname": user_2.surname,
             },
-            {
-                "senderName": "Иван",
-                "text": "Тоже хорошо, спасибо!"
-            }
-        ]
-    }
-    return {'Диалога еще нет'}
+            "messages": [{
+                "senderId": message.sender_id,
+                "senderName": user_2.name if message.sender_id == user_2.id else user.name,
+                "text": message.content,
+                "timestamp": message.date_time.strftime('%d.%m.%Y %H:%M')
+            } for message in from_orm]
+        }
+
+        
+
 
     
+@main_router.post('/messages')
+async def send_message(
+    message: MessageCreate, 
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+    user: Annotated[User, Depends(get_current_user)]):
+  
+    current_date = datetime.now()
+    await ChatRepository.add(
+        session=session,
+        sender_id=user.id,
+        content=message.content,
+        recipient_id=message.recipient_id,
+        date_time=current_date
+    )
+    message_data: dict[str, User | MessageCreate] = {
+        'sender_id': user.id,
+        'recipient_id': message.recipient_id,
+        'content': message.content,
+        'timestamp': current_date.strftime('%d.%m.%Y %H:%M'),
+        'sender_name': user.name
+    }
+
+    await notify_user(user.id, message_data)
+    # await notify_user(message.recipient_id, message_data)  #
+        
+    return message_data
+  
+
+async def notify_user(user_id: int, message: dict):
+    if user_id in active_connections:
+        websocket = active_connections[user_id]
+        try:
+            await websocket.send_json(message)
+            logger.info(f'Сообщение отправлено пользователю {user_id}: сообщение - {message}')
+        except Exception as e:
+            logger.error(f'Ошибка отправки сообщения пользователю: {user_id}: {str(e)}')
+    else:
+        logger.warning(f'Пользователь: {user_id} не подключен')
+
+
+
+
+@main_router.websocket("/ws/messages/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: int):
+    await websocket.accept()
+    active_connections[user_id] = websocket
+    try:
+        while True:
+            data = await websocket.receive_text()
+            logger.info(f'Получено сообщение от пользователя: {user_id}: {data}')
+    except WebSocketDisconnect as e:
+        logger.error(f'Ошибка в соединении для пользователя: {user_id}: {str(e)}')
+    finally:
+        active_connections.pop(user_id, None)
+        logger.info(f'Пользователь: {user_id} отключился')
