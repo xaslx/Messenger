dc52c072fda578a342d7157b709b3eba7883a0fa
Maksim <knyazev.maksim2852@gmail.com>
2024-10-18 20:26:14 +0400

    update model

--- src/models/chat.py
+++ src/models/chat.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Integer, Text, ForeignKey
+from sqlalchemy import ForeignKey, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
-
+from datetime import datetime
 
 class Message(Base):
     __tablename__ = 'messages'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    sender_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    recipient_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    content: Mapped[str] = mapped_column(Text)
+    sender_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    recipient_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    content: Mapped[str] = mapped_column(String(1000))
+    date_time: Mapped[datetime] = mapped_column(DateTime)
