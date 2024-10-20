74395e85e102b9920f642def6f41447b3aff7d85
Maksim <knyazev.maksim2852@gmail.com>
2024-10-20 21:42:42 +0400

    update exceptions

---
+++ exceptions.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException, status
+
+
+class BaseException(HTTPException):
+    status_code = 500
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+# Пользователи
+class UserAlreadyExistsException(BaseException):
+    status_code = 409
+    detail = "Пользователь уже существует"
+
+
+class IncorrectEmailOrPasswordException(BaseException):
+    status_code = 401
+    detail = "Неверный email или пароль"
+
+
+class IncorrectEmailOrPasswordExceptionNotEn(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Email или пароль должны быть на английском"
+
+
+class UserNotFound(BaseException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Пользователь не найден"
+
+
+# JWT token
+class TokenExpiredException(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Токен истёк"
+
+
+class TokenAbsentException(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Токен отсутствует"
+
+
+class IncorrectTokenException(BaseException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Неверный формат токена"
