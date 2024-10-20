c75c0fa133cce1a040d2287d30564d40048fc3bb
Maksim <knyazev.maksim2852@gmail.com>
2024-10-20 21:41:06 +0400

    add comments

--- main.py
+++ main.py
@@ -43,16 +43,21 @@ async def update_online_status(
     request: Request, 
     call_next, 
     session: AsyncSession = Depends(get_async_session)):
+
+    '''При каждом запросе user_id записывается в редис на 1 минуту для того чтобы отслеживать что он онлайн'''
+
     token = request.cookies.get('user_access_token')
     response = await call_next(request)
     if token:
         user = await get_current_user(async_db=session, token=token)
         if user:
-            await redis.set(f"user:{user.id}:is_online", "1", ex=300)
+            await redis.set(f"user:{user.id}:is_online", "1", ex=60)
         return response
     return response
 
 
+
 @app.exception_handler(404)
 async def http_exception_handler(request, exc):
+    #редирект в случае если не нашелся путь
     return RedirectResponse(url="/messages")
