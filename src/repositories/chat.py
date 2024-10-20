41b5469c14cd94dec95c46178889c509ea5e5540
Maksim <knyazev.maksim2852@gmail.com>
2024-10-20 21:45:00 +0400

    update method

--- src/repositories/chat.py
+++ src/repositories/chat.py
@@ -1,6 +1,20 @@
 from src.repositories.sqlalchemy import SQLAlchemyRepository
 from src.models.chat import Message
+from sqlalchemy import select, or_, and_
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class ChatRepository(SQLAlchemyRepository):
     model: Message = Message
+
+
+    @classmethod
+    async def get_messages_between_users(cls, user_id_1: int, user_id_2: int, session: AsyncSession):
+        query = select(cls.model).filter(
+            or_(
+                and_(cls.model.sender_id == user_id_1, cls.model.recipient_id == user_id_2),
+                and_(cls.model.sender_id == user_id_2, cls.model.recipient_id == user_id_1)
+            )
+        ).order_by(cls.model.id)
+        result = await session.execute(query)
+        return result.scalars().all()
--- src/repositories/sqlalchemy.py
+++ src/repositories/sqlalchemy.py
@@ -9,44 +9,35 @@ from sqlalchemy.exc import SQLAlchemyError
 class SQLAlchemyRepository(AbstractRepository):
 
     model = None
-
-    def __init__(self, session: AsyncSession):
-        self.session = session
  
 
-    async def add(self, **data: dict):
+    @classmethod
+    async def add(self, session: AsyncSession, **data: dict,):
         try:
             stmt = insert(self.model).values(**data).returning(self.model.__table__.columns)
-            res = await self.session.execute(stmt)
-            await self.session.commit()
+            res = await session.execute(stmt)
+            await session.commit()
             return res.mappings().one_or_none()
         except (SQLAlchemyError, Exception) as e:
             logger.error(
                 'Ошибка при добавлении записи в базу данных', extra={'данные': data, 'ошибка': e}
             )
 
-
-    async def find_one_or_none(self, **filter_by):
+    @classmethod
+    async def find_one_or_none(self, session: AsyncSession, **filter_by, ):
         try:
             stmt = select(self.model).filter_by(**filter_by)
-            res = await self.session.execute(stmt)
+            res = await session.execute(stmt)
             return res.scalar_one_or_none()
         except (SQLAlchemyError, Exception) as e:
             logger.error(f'Ошибка при поиске значения в базе данных', extra={'ошибка': e})
 
 
-    async def find_all(self, **filter_by):
+    @classmethod
+    async def find_all(self, session: AsyncSession, **filter_by):
         try:
             stmt = select(self.model).filter_by(**filter_by)
-            res = await self.session.execute(stmt)
-            return res.scalar_one_or_none()
+            res = await session.execute(stmt)
+            return res.scalars().all()
         except (SQLAlchemyError, Exception) as e:
             logger.error(f'Ошибка при поиске всех значений в базе данных', extra={'ошибка': e})
-
-    async def delete(self, id: int) -> int:
-        try:
-            stmt = delete(self.model).filter_by(id=id).returning(self.model.id)
-            await self.session.execute(stmt)
-            await self.session.commit()
-        except (SQLAlchemyError, Exception) as e:
-            logger.error(f'Ошибка при удалении значения из базы данных', extra={'ошибка': e})
